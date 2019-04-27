18d502f2cecdac8bfcd0d5c9cd086d2239d50a72
Manuel Grundner <m.grundner@delegate.at>
2019-04-27 12:43:30 +0200

    feat: added win module

---
+++ src/Xaf.Module.Win/content/ModuleNameWindowsFormsModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Win.SystemModule;
+
+namespace Scissors.Modules.ModuleName.Win
+{
+    public class ModuleNameWindowsFormsModule : ModuleBase
+    {
+        protected override ModuleTypeList GetRequiredModuleTypesCore() => new ModuleTypeList(new[]
+        {
+            typeof(SystemModule),
+            typeof(SystemWindowsFormsModule),
+        });
+    }
+}
--- src/Xaf.Module/content/ModuleNameModule.cs
+++ src/Xaf.Module/content/ModuleNameModule.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
 
 namespace Scissors.Modules.ModuleName
 {
     public class ModuleNameModule : ModuleBase
     {
+        protected override ModuleTypeList GetRequiredModuleTypesCore() => new ModuleTypeList(new[]
+        {
+            typeof(SystemModule)
+        });
     }
 }
