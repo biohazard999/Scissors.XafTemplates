e523a92fed49e15b8d4f1e52ffbea81679dc46f3
Manuel Grundner <m.grundner@delegate.at>
2019-04-28 14:30:09 +0200

    feat: added template stub for win
    #2

---
+++ src/Xaf.Win/content/ModuleName.Win/ModuleNameWindowsFormsModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Win.SystemModule;
+using Scissors.Modules.ModuleName;
+
+namespace Scissors.Modules.ModuleName.Win
+{
+    public class ModuleNameWindowsFormsModule : ModuleBase
+    {
+        protected override ModuleTypeList GetRequiredModuleTypesCore() => new ModuleTypeList(new[]
+        {
+            typeof(SystemModule),
+            typeof(SystemWindowsFormsModule),
+            typeof(ModuleNameModule)
+        });
+    }
+}
